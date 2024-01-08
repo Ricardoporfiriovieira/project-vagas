85982cfa2e4de82064c202e27c2f239b46ac0b41
Ricardoporfiriovieira <ricardoporfiriovieira@gmail.com>
2024-01-08 19:56:28 -0300

    update

--- src/components/layouts/Content.vue
+++ src/components/layouts/Content.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         Conteudo
+        <Home></Home>
+        <PublicarVaga></PublicarVaga>
     </div>
 </template>
 
 <script>
-/* eslint-disable vue/multi-word-component-names */
+    import Home from '../views/Home.vue'
+    import PublicarVaga from '../views/PublicarVaga.vue'
+
+    export default {
+        components: {
+            Home,
+            PublicarVaga
+        }
+    }
+
 </script>
 
 <style>
